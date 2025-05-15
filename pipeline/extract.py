0c936d3a5041514e10e5fd07e327ee8aeff65a95
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-05-16 00:15:34 +0200

    dataframe fix

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -4,6 +4,14 @@ import pandas as pd
 LOG_PATH = 'data/logfiles.log'
 LOG_PATTERN = r'(?P<ip>\S+) - - \[(?P<time>[^\]]+)\] "(?P<method>\S+) (?P<endpoint>\S+) HTTP/\d\.\d" (?P<status>\d+) (?P<bytes>\d+) "(?P<referrer>[^"]+)" "(?P<ua>[^"]+)" (?P<response_time>\d+)'
 
+def droptime(df):
+    """
+    Drop time
+    
+    It's all the same
+    """
+    df.drop(["time"], axis=1)
+
 def parse_logs(path):
     data = []
     with open(path) as f:
@@ -11,7 +19,8 @@ def parse_logs(path):
             match = re.match(LOG_PATTERN, line)
             if match:
                 data.append(match.groupdict())
-    return pd.DataFrame(data)
+    
+    return droptime(pd.DataFrame(data))
 
 if __name__ == "__main__":
     df = parse_logs(LOG_PATH)
