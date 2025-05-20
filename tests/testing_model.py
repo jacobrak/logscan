b41b76aa9bf51b21c567cabcdb9bf6e0f2a75745
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-05-21 00:23:42 +0200

    testing models unittest

--- tests/testing_model.py
+++ tests/testing_model.py
@@ -5,18 +5,27 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scripts.random_forest_ import RandomForestRegressor_model
+import pandas as pd
+
+columns = ['bytes', 'request_type_dummy', 'endpoint_dummy', 'is_error',
+       'os_Mac OS X', 'os_Windows', 'os_iOS', 'd_Mac',
+       'd_OnePlus ONEPLUS A6000', 'd_Other', 'd_iPhone', 'b_Chrome Mobile',
+       'b_Edge', 'b_Edge Mobile', 'b_Firefox', 'b_Firefox Mobile',
+       'b_Mobile Safari', 'b_Opera', 'b_Opera Mobile', 'b_Safari']
+sample_input = pd.DataFrame(
+    [[4944,803,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1]],
+    columns=columns
+)
 
 class TestDataCreation(unittest.TestCase):
     def setUp(self):
         self.model = load("models/random_forest_model.joblib")
 
     def test_model_prediction_shape(self):
-        sample_input = np.array([[4944,803,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1]])
         prediction = self.model.predict(sample_input)
         self.assertEqual(prediction.shape, (1,))
 
     def test_same_model(self):
-        sample_input = np.array([[4944,803,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1]])
         prediction = self.model.predict(sample_input)
         model2 = RandomForestRegressor_model()
         prediction1 = model2.predict(sample_input)
