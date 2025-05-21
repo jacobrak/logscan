7de45c7052e977dc8e70d237f2de88a90d2c5684
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-05-21 22:17:25 +0200

    train_test for airflow

--- scripts/train_test_spliting.py
+++ scripts/train_test_spliting.py
@@ -4,8 +4,15 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+def get_data_path():
+    if os.path.exists("/app/data/processed_data.csv"):
+        return "/app/data/processed_data.csv"
+    else:
+        return "data/processed_data.csv"
+    
 def prepare_data():
-    df = pd.read_csv("data/processed_data.csv")
+    data_path = get_data_path()
+    df = pd.read_csv(data_path)
 
     X = df.drop(["response_time"], axis=1)
     y = df["response_time"]
